cc3c1a9a346e557b773fbba817890bbf9d680e86
io303 <srahul12768@gmail.com>
2025-11-01 20:28:21 +0530

    learn

---
+++ customReac/customreact.js
@@ -0,0 +1,31 @@
+
+function cunstomRender(reactElement,container){
+    // const domElement=document.createElement(reactElement.type)
+    //   domElement.innerHTML = reactElement.children
+    // domElement.setAttribute('href', reactElement.props.href)
+    // domElement.setAttribute('target', reactElement.props.target)
+
+  const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.children
+    for(const prop in reactElement.props){
+        if(prop=='children'){
+            continue;
+        }
+        domElement.setAttribute(prop,reactElement.props[prop])
+    }
+      container.appendChild(domElement)
+
+
+}
+
+const reactElement ={
+    type:'a',
+    props:{
+        href:'https://google.com',
+        target:'_blank'
+    },
+    children :'click mw to  visit google'
+}
+ const mainContainer=document.getElementById("root");
+
+ cunstomRender(reactElement,mainContainer)
